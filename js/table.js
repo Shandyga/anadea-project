f5ff3b3126737aea0d7114fba93cad52de06b13e
Shandyga <shana.vitaliy@gmail.com>
2017-11-11 15:03:40 +0200

    Move js

---
+++ js/table.js
@@ -0,0 +1,22 @@
+function generateTable(number) {
+	let table = '<table>'
+
+	for(let i = 1; i <= number; i++) {
+		table += "<tr>"
+
+		for (let j = 1; j <= number; j++ ) {
+			table += "<td>" + j
+		}
+
+		table += "</td>"
+	}
+
+	table += '</table>'
+
+	return table;
+}
+
+function insertTable(el) {
+	number = parseInt( el[el.selectedIndex].value )
+	document.getElementById('container').innerHTML = generateTable(number)
+}
